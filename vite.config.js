da6fa4a609623021f2985a527463a14d13a7a340
Kovynx <kovynm@gmail.com>
2026-09-10 19:31:23 -0500

    retoques

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Permite el despliegue directo en GitHub Pages o cualquier subruta
 })
